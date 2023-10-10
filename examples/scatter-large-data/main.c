e56ae7d6ba70d9c23127b12148a85660e8aa907c
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2023-10-10 12:29:26 +0900

    Fix scatter sample

--- examples/scatter-large-data/main.c
+++ examples/scatter-large-data/main.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N_DATA 1000000
+#define N_SEND 12000
+#define N_RECV 1000
 #define N_STEP 1000
 
-int64_t communicate_large_data_and_calc(int64_t *data, size_t datasize,
-                                        int rank, int size);
+int64_t bench(int64_t *sendbuf, size_t sendsize, int64_t *recvbuf,
+              size_t recvsize, int rank, int size);
 
 int main(int argc, char *argv[]) {
-    int64_t data[N_DATA];
+    int64_t sendbuf[N_SEND];
+    int64_t recvbuf[N_RECV];
     int rank, n_process;
 
-    int provided_thread_support_level;
     MPI_Init(&argc, &argv);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -20,32 +21,33 @@ int main(int argc, char *argv[]) {
 
     for (size_t i = 1; i <= N_STEP; i++) {
         int64_t sum =
-            communicate_large_data_and_calc(data, N_DATA, rank, n_process);
+            bench(sendbuf, N_SEND, recvbuf, N_RECV, rank, n_process);
+        printf("step %ld done\n", i);
     }
 
     MPI_Finalize();
     return 0;
 }
 
-int64_t communicate_large_data_and_calc(int64_t *data, size_t datasize,
-                                        int rank, int size) {
+int64_t bench(int64_t *sendbuf, size_t sendsize, int64_t *recvbuf,
+              size_t recvsize, int rank, int size) {
     if (rank == 0) {
-        for (size_t i = 0; i < datasize; i++) {
-            data[i] = i;
+        for (size_t i = 0; i < sendsize; i++) {
+            sendbuf[i] = i;
         }
     }
 
-    MPI_Scatter(data, datasize / size, MPI_INT64_T, data, datasize / size,
+    MPI_Scatter(sendbuf, recvsize, MPI_INT64_T, recvbuf, recvsize,
                 MPI_INT64_T, 0, MPI_COMM_WORLD);
 
     int64_t local_sum = 0;
-    for (size_t i = 0; i < datasize / size; i++) {
-        local_sum += data[i];
+    for (size_t i = 0; i < recvsize; i++) {
+        local_sum += recvbuf[i];
     }
 
     int64_t *local_sums = malloc(size * sizeof(int64_t));
-    MPI_Gather(&local_sum, 1, MPI_INT64_T, local_sums, 1, MPI_INT64_T, 0,
-               MPI_COMM_WORLD);
+    MPI_Gather(&local_sum, 1, MPI_INT64_T, local_sums, 1, MPI_INT64_T,
+               0, MPI_COMM_WORLD);
 
     int64_t sum = 0;
     if (rank == 0) {
